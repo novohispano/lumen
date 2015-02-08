ff72273a46b2575e9c773674e51566e47c761c50
novohispano <novohispano@gmail.com>
2015-02-08 11:29:37 -0700

    Fix admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :dashboard, only: [:show]
     resources :users
+
+    get '/', to: 'dashboard#show'
   end
 
   root 'site#show'
