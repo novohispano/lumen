2128da23a7dcfbb417df6fde3ab103389d2d027f
novohispano <novohispano@gmail.com>
2015-02-08 11:50:46 -0700

    Add admin/sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-
+    resources :sessions, only: [:new, :create, :destroy]
+    
     get '/', to: 'dashboard#show'
   end
 
