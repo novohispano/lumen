1737db1e844e77ba48f6e9d897ad2f504f41ba6f
novohispano <novohispano@gmail.com>
2015-02-22 18:03:29 -0700

    Style dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Admin::DashboardController < Admin::AdminController
   before_action :require_user!
-  
+
   def show
+    @projects = %w(blah hello)
+    @news     = %w(blah hello)
+    @photos   = Photo.all
+    @users    = User.all
   end
 end
