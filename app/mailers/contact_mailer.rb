b2d75f59c3dc3006d9473758c51344f0d8a71284
novohispano <novohispano@gmail.com>
2015-08-02 16:03:28 -0600

    Add email templates

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,10 @@
 class ContactMailer < ApplicationMailer
-  include ActionView::Helpers::TextHelper
-
   def contact_email(data)
     @data = OpenStruct.new(data)
 
     mail(
       from:    format_sender(@data),
       subject: @data.subject,
-      body:    simple_format(@data.message)
       )
   end
 
